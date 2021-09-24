be22b12a12ad691c27dcb016b998f56e956497fd
Michael <michaelgrant13@hotmail.co.uk>
2021-09-24 20:53:22 +0100

    Finished Pandas section

--- TutorialDataManipluationNumpyPandas/IntroPandas.py
+++ TutorialDataManipluationNumpyPandas/IntroPandas.py
@@ -109,3 +109,96 @@ data.rename(index = str.upper, columns= str.title, inplace= True)
 print(data)
 
 print("\nHow to categorize (bin) continuous variables\n")
+ages = [20, 22, 25, 27, 21, 23, 37, 31, 61, 45, 41, 32]
+bins = [18, 25, 35, 60, 100]
+
+cats = pd.cut(ages, bins)
+print("\nWe'll divide the ages into bins such as 18-25, 26-35,36-60 and 60 and above.\n'(' means the value is included in the bin, '[' means the value is excluded")
+print("\n", cats)
+
+print("\nTo include the right bin value, we can do:")
+print(pd.cut(ages,bins,right=False))
+
+print("\npandas library intrinsically assigns an encoding to categorical variables.")
+print(cats.codes)
+
+print("\nLet's check how many observations fall under each bin")
+print(pd.value_counts(cats))
+
+# We can pass a unique name to each label
+bin_names = ['Youth', 'YoungAdult', 'MiddleAge', 'Senior']
+new_cats = pd.cut(ages, bins, labels=bin_names)
+print("\n", pd.value_counts(new_cats))
+
+print("\nWe can also calculate their cumulative sum!\n")
+print(pd.value_counts(new_cats).cumsum())
+
+"""grouping data and creating pivots in pandas."""
+print("\ngrouping data and creating pivots in pandas.\n")
+
+df = pd.DataFrame({'key1' : ['a', 'a', 'b', 'b', 'a'],
+                   'key2' : ['one', 'two', 'one', 'two', 'one'],
+                   'data1' : np.random.randn(5),
+                   'data2' : np.random.randn(5)})
+
+print(df)
+
+print("\nCalculate the mean of data1 column by key1")
+grouped = df['data1'].groupby(df['key1'])
+print(grouped.mean())
+
+print("\nlet's see how to slice the data frame\n")
+dates = pd.date_range('20130101', periods= 6)
+df = pd.DataFrame(np.random.randn(6, 4), index= dates, columns= list('ABCD'))
+print(df)
+
+print("\nGet the first n rows from the date frame")
+print(df[:3])
+
+print("\nSlice based on the Dates!\n")
+print(df['20130101':'20130104'])
+
+print("\nSliced based on Column names\n")
+print(df.loc[:, ['A','B']])
+
+print("\nWe can slice on row index labels and column names\n")
+print(df.loc['20130102':'20130103',['A','B']])
+
+print("\nSlice based on index of columns\n")
+print(df.iloc[3]) #returns 4th row (index is 3rd)
+
+print("\nreturns a specific range of rows")
+print(df.iloc[2:4, 0:2])
+
+print("\nBoolean indexing based on column values as well. This helps in filtering a data set based on a pre-defined condition.\n")
+print(df[df.A > 1])
+
+print("\nWe can copy the dataset")
+df2 = df.copy()
+df2['E'] = ['one', 'one', 'two', 'three', 'four', 'three']
+print(df2)
+
+print("\nselect rows based on column values\n")
+print(df2[df2['E'].isin(['two','four'])])
+
+print("\nselect all rows except those with two and four\n")
+print(df2[~df2['E'].isin(['two', 'four'])])
+
+print("\nlist all columns where A is greater than C\n")
+print(df.query('A > C'))
+
+print("\nQuery using the OR condition\n")
+print(df.query('A < B | C > A'))
+
+print("\nPivot tables are extremely useful in analyzing data using a customized tabular format. It offers a super-quick way to analyze data.\n")
+# Create a data frame
+data = pd.DataFrame({'group': ['a', 'a', 'a', 'b','b', 'b', 'c', 'c','c'],
+                 'ounces': [4, 3, 12, 6, 7.5, 8, 3, 5, 6]})
+print(data)
+
+print("\nCalculate the means of each group")
+print(data.pivot_table(values= 'ounces', index= 'group', aggfunc=np.mean))
+
+print("\nCalculate count by each group\n")
+print(data.pivot_table(values= 'ounces', index= 'group', aggfunc= 'count'))
+
